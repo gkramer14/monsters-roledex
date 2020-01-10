dd2f0e1b16f290c4d8b2d2b2e15366cb2b60c52d
Guilherme Kramer <guilherme.kramer98@gmail.com>
2020-01-09 22:31:14 -0300

    Updates

---
+++ precache-manifest.605c2c886c1f7e3a95ac5fbdcb6e25d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd5965c58000d54448a5c67f42fafb6e",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "4ac14dc6dcf5a42ff568",
+    "url": "/monsters-roledex/static/css/main.1aae008e.chunk.css"
+  },
+  {
+    "revision": "fd6ebc4e82ad40ffe41b",
+    "url": "/monsters-roledex/static/js/2.c31d5ea1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-roledex/static/js/2.c31d5ea1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ac14dc6dcf5a42ff568",
+    "url": "/monsters-roledex/static/js/main.1773337d.chunk.js"
+  },
+  {
+    "revision": "1153fd5fb0d3fbb24a18",
+    "url": "/monsters-roledex/static/js/runtime-main.62d407ea.js"
+  }
+]);
